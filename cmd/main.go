e555105ce5485ac260776550a0a5b5f61ff2a48b
Jackson <hackisonjd@users.noreply.github.com>
2025-03-26 19:11:55 -0400

    fixed testing, one feature at a time

--- cmd/main.go
+++ cmd/main.go
@@ -2,20 +2,8 @@ package main
 
 import (
 	"fmt"
-	"os"
-
-	"github.com/hackisonjd/http-server/pkg/http"
 )
 
 func main() {
-	// Init a new server on just localhost interfaces for now
-	fmt.Println("creating new server!")
-	server := http.NewServer("127.0.0.1", "8080")
-
-	fmt.Println("starting the server")
-	// Start the server
-	if err := server.ListenAndServe(); err != nil {
-		fmt.Printf("could not start server: %v\n", err)
-		os.Exit(1)
-	}
+	fmt.Println("this is just a dummy file for now, implementation will come soon")
 }
